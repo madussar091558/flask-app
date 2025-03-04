0a4be8d17c3c0ddca10ceb6de98241218be6f7dc
madussar091558 <madussarmm@gmail.com>
2025-03-04 12:22:26 +0500

    ap.py chck proxey

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import os
+import requests
 from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
+
+# Use a proxy (Replace with a working one)
+PROXIES = {
+      "http": "http://dahydzuv-rotate:qi0v0hqggj55@p.webshare.io:80/",
+        "https": "http://dahydzuv-rotate:qi0v0hqggj55@p.webshare.io:80/"
+}
 
 app = Flask(__name__)
 
@@ -8,10 +16,10 @@ def get_transcript():
     video_id = request.args.get("video_id")
 
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies=PROXIES)
         return jsonify({"transcript": transcript})
     except Exception as e:
-        return jsonify({"error": str(e)})  # Log the exact error
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
